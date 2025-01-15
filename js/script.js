1550796a36bd873e748b8c0ab7d39fb72a2e32b8
saidwafi-rpl <saidlesmana28@gmail.com>
2025-01-15 22:51:38 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs((slideIndex += n));
-}
-
-function showDivs(n) {
-    var i;
-    var imgList = document.getElementsByClassName("img-slideshow");
-    if (n > imgList.length) slideIndex = 1;
-    else if (n < 1) slideIndex = imgList.length;
-
-    for(i = 0; i < imgList.length; i++) {
-        imgList[i].style.display = "none";
-    }
-
-    imgList[slideIndex - 1].style.display = "block";
-}
-
-setInterval(() => {
-    plusDivs(1);
-}, 3000)
-
 function validateForm() {
-    const name = document.forms["message-form"]["full-name"].value;
-    const birthDate = document.forms["message-form"]["birth-date"].value;
-    const gender = document.forms["message-form"]["gender"].value;
-    const messages = document.forms["message-form"]["messages"].value;
+  let nameInput = document.getElementById("name-input").value;
 
-    if (name == "" || birthDate == "" || gender == "" || messages == "") {
-        alert("Tidak boleh ada yang kosong!");
-        return false;
-    }
-
-    setSenderUI(name, birthDate, gender, messages);
-
-    return false;
+  console.log(nameInput);
+  if (nameInput == "") {
+    alert("Nama tidak boleh kosong!");
+  } else {
+    // Result form
+    document.getElementById("result-form").innerHTML = nameInput;
+  }
 }
+
+document.getElementById("submit-btn").addEventListener("click", validateForm);
 
 function setSenderUI(name, birthDate, gender, messages) {
-    document.getElementById("sender-full-name").innerHTML = name;
-    document.getElementById("sender-birth-date").innerHTML = birthDate;
-    document.getElementById("sender-gender").innerHTML = gender;
-    document.getElementById("sender-messages").innerHTML = messages;
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthDate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
 }
+
+let indexBanner = 0;
+
+changeBackground();
+
+function nextBanner() {
+    indexBanner = indexBanner + 1;
+
+    changeBackground();
+}
+
+// Function Banner Autoslide
+function changeBackground() {
+    let bannerList = document.getElementsByClassName("banner-image");
+    console.log(bannerList.length);
+
+    console.log(indexBanner);
+    if (indexBanner > bannerList.length - 1) {
+        // Reset indexBanner
+        indexBanner = 0;
+    }
+
+    // Looping Banner Autoslide
+    for (let i = 0; i < bannerList.length; i++) {
+        bannerList[i].style.display = "none";
+    }
+
+    bannerList[indexBanner].style.display = "block";
+}
+
+setInterval(nextBanner, 3000);
